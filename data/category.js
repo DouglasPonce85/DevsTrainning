f1589a7c49728e54c2424e7ca0d3ca006e3e2dfb
pires190286 <pires190286@hotmail.com>
2019-07-26 22:50:43 -0600

    Adding Data Category Mock

---
+++ data/category.js
@@ -0,0 +1,24 @@
+const category = [
+    {
+        category_id: 1,
+        name: "Linea Blanca",
+        is_active: true
+    },
+    {
+        category_id: 2,
+        name: "Deportes",
+        is_active: true
+    },
+    {
+        category_id: 3,
+        name: "Temporada",
+        is_active: true
+    },
+    {
+        category_id: 4,
+        name: "TVs",
+        is_active: false
+    }
+];
+
+module.exports = category;
